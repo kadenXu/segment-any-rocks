ea0efab9e75ae1adf5523fbd2da236d0a8290c23
Kaden Xu <kadenxu.ai@gmail.com>
2025-05-09 11:41:16 -0400

    Modify persisting method

--- my_sam_manager.py
+++ my_sam_manager.py
@@ -5,10 +5,10 @@ import os
 class MySamManager:
     def __init__(self, image_obj, dir=".", storage=None):
         self.image = image_obj["image"]
-        self.filename = image_obj["filename"]
+        self.filename = image_obj["filename"][:-4]
         self.mask_image = np.zeros((self.image.shape[:-1]), dtype=bool)
         self.mask_images = list()
-        self.storage = os.path.join(dir, "labels", self.filename) if storage is None else storage
+        self.storage = storage if storage else os.path.join(dir, "labels")
 
     def store_mask(self, mask):
         self.mask_images.append(mask)  
@@ -17,8 +17,10 @@ class MySamManager:
         self.mask_image |= mask_bool
 
     def persist(self):
-        with open(self.storage, "wb") as out_file:
-            for mask in self.mask_image:
+        if not (os.path.exists(self.storage) and os.path.isdir(self.storage)):
+            os.makedirs(self.storage)
+        with open(os.path.join(self.storage, self.filename), "wb") as out_file:
+            for mask in self.mask_images:
                 np.save(out_file, mask)
     
     def exists(self, x, y):
